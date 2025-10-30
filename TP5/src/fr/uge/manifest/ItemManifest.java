7a62f2e003ce8059c3e14d7d94bc1a11e6f741ad
Davideuh <davidliv6@gmail.com>
2025-10-30 18:26:44 +0100

    ajout gitignore

--- TP5/src/fr/uge/manifest/ItemManifest.java
+++
@@ -1,9 +0,0 @@
-package fr.uge.manifest;
-
-public interface ItemManifest {
-	public abstract int price();
-	public abstract String toString();
-	public abstract String getDestination();
-	public abstract String getId();
-	
-}
